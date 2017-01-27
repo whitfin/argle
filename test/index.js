d9a8c3f1ab37d3635bb10f7483e707fd50ebb235
Isaac Whitfield <iw@zackehh.com>
2017-01-27 10:26:38 -0800

    Node added spread/rest in Node v5

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ suite('Argle', function () {
 
   require('./argle.test');
 
-  if (Number(process.versions.node.split('.')[0] || 0) >= 4) {
+  if (Number(process.versions.node.split('.')[0] || 0) > 4) {
     require('./argle.test.es6');
   }
 
